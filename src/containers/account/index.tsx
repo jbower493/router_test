5a5667d7ce2c7e8c3a93a0398962122bfac4b84f
Jamie Bower <jbower@nexigen.digital>
2024-06-21 09:25:06 +1000

    feat: get it animating between 2 different children based on key

--- src/containers/account/index.tsx
+++ src/containers/account/index.tsx
@@ -1,12 +1,28 @@
 import { createRoute } from "@tanstack/react-router";
+import { useState } from "react";
 import { rootRoute } from "../../main";
+import { MyAnimate } from "../../utils/MyAnimate";
 
 function Account() {
-    return <h1>Account</h1>;
+  const [isVisible, setIsVisible] = useState(true);
+
+  return (
+    <div>
+      <h1>Account</h1>
+      <button type="button" onClick={() => setIsVisible((prev) => !prev)}>
+        Toggle
+      </button>
+      <MyAnimate>
+        <div>
+          {isVisible ? <h3 key="1">Animate</h3> : <h1 key="2">Other thing</h1>}
+        </div>
+      </MyAnimate>
+    </div>
+  );
 }
 
 export const accountRoute = createRoute({
-    getParentRoute: () => rootRoute,
-    path: "/account",
-    component: Account,
+  getParentRoute: () => rootRoute,
+  path: "/account",
+  component: Account,
 });
--- src/utils/MyAnimate.tsx
+++ src/utils/MyAnimate.tsx
@@ -1,42 +1,45 @@
 import { cloneElement, useLayoutEffect, useRef, useState } from "react";
 import "./animate.css";
 
-export function MyAnimate({ children }: { children: any }) {
-    const [, force] = useState(false);
+export function MyAnimate({ children }: { children: JSX.Element }) {
+  const [, force] = useState(false);
+  const [state, setState] = useState<"entering" | "exiting" | "stable">(
+    "stable"
+  );
 
-    const hasMounted = useRef(false);
-    const hasUnmounted = useRef(false);
-    const currentChild = useRef(null);
-    const exitingChild = useRef(null);
+  const hasMounted = useRef(false);
+  const currentChild = useRef<JSX.Element | null>(null);
+  const exitingChild = useRef<JSX.Element | null>(null);
+  const enteringChild = useRef<JSX.Element | null>(null);
 
-    useLayoutEffect(() => {
-        hasMounted.current = true;
-    }, []);
+  useLayoutEffect(() => {
+    if (children.key !== currentChild.current?.key && hasMounted.current) {
+      exitingChild.current = currentChild.current;
+      enteringChild.current = children;
+      setState("exiting");
+    }
+    currentChild.current = children;
+  }, [children?.key]);
 
-    useLayoutEffect(() => {
-        if (!children && hasMounted) {
-            exitingChild.current = currentChild.current;
-            hasUnmounted.current = true;
-            force((prev) => !prev);
-        }
-        currentChild.current = children;
-    }, [children]);
+  useLayoutEffect(() => {
+    hasMounted.current = true;
+  }, []);
 
-    const exitingChildElement = exitingChild.current
-        ? cloneElement(exitingChild.current, {
-              className: "motion-out",
-              onAnimationEnd: () => {
-                  exitingChild.current = null;
-                  force((prev) => !prev);
-              },
-          })
-        : null;
+  const exitingChildElement = exitingChild?.current
+    ? cloneElement(exitingChild.current, {
+        className: "motion-out",
+        onAnimationEnd: () => {
+          exitingChild.current = null;
+          setState("entering");
+        },
+      })
+    : null;
 
-    const childElement = children
-        ? cloneElement(children, {
-              className: hasUnmounted.current ? "motion-in" : "",
-          })
-        : null;
+  const childElement = children
+    ? cloneElement(children, {
+        className: "motion-in",
+      })
+    : null;
 
-    return childElement || exitingChildElement;
+  return state === "exiting" ? exitingChildElement : childElement;
 }
