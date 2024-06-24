fbb40cf554858188ca20206f34c6dca63e7580f7
Jamie Bower <jbower@nexigen.digital>
2024-06-24 10:24:51 +1000

    chore: write notes

--- src/components/AnimatePresence/index.tsx
+++ src/components/AnimatePresence/index.tsx
@@ -17,6 +17,26 @@ import { AnimatePresenceProvider } from "./provider";
  * - All "AnimatePresence.Child" instances must have a "parentId" which matches the "id" of the closest "AnimatePresence" up the tree
  *
  * - All elements in between "AnimatePresence" and "AnimatePresence.Child" will not be rendered while "AnimatePresence.Child" is exiting, so you should only use components that render no dom elements in between the parent and child.
+ *
+ *
+ *
+ * Testing and fixes required:
+ *
+ *
+ * Fixes:
+ *
+ * - Use another context, one for each level of "AnimatePresence", to auto increment the id's, so that we don't have to explicitly pass "id" and "parentId"
+ *
+ * - Have the "AnimatePresence.Child" create it's own key, so that the dom element to be exited doesn't have to have a unique key. The AnimatePresence.Child can then send this key back to "localSetCurrentChild" to be stored with the react element
+ *
+ * - Create a small abstraction on my thing, to include an outlet. Something like "AnimatedOutlet"
+ *
+ * Testing:
+ *
+ * - Test pending route (using "loader")
+ *
+ * - Test with suspense
+ *
  */
 
 function _AnimatePresence({
